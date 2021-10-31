48950ac5daf82fdfbaa1fcd927623961b01ea764
Nele Hirsch <nele@ebildungslabor.de>
2021-10-31 15:48:20 +0100

    Add files via upload

---
+++ js/blaetter.js
@@ -0,0 +1,42 @@
+var blaetter = document.getElementById('blaetter');
+var blaetterBtn = document.getElementById('blaetter-btn');
+
+function playPause(song){
+   if (song.paused && song.currentTime >= 0 && !song.ended) {
+      song.play();
+   } else {
+      song.pause();
+   }
+}
+
+function reset(btn, song){
+   if(btn.classList.contains('playing')){
+      btn.classList.toggle('playing');
+   }
+   song.pause();
+   song.currentTime = 0;
+}
+
+function progress(btn, song){
+   setTimeout(function(){
+      var end = song.duration; 
+      var current = song.currentTime;
+      var percent = current/(end/100);
+      //check if song is at the end
+      if(current==end){
+         reset(btn, song);
+      }
+      //set inset box shadow
+      btn.style.boxShadow = "inset " + btn.offsetWidth * (percent/100) + "px 0px 0px 0px rgba(0,0,0,0.125)"
+      //call function again
+      progress(btn, song);     
+   }, 1000);
+}
+
+blaetterBtn.addEventListener('click', function(){
+   blaetterBtn.classList.toggle('playing');
+   playPause(blaetter);
+   progress(blaetterBtn, blaetter);
+});
+
+
